9d12e91a7872e0f046ead9023e3cd1f2605ad409
Admin <Admin@HOME-PC>
2023-02-27 21:34:35 +0300

    Shape

---
+++ IntroductionToOOP/Inheritance/Geometry/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+enum Color
+{
+    console_blue = 0x99,
+    console_green = 0xAA,
+    console_red = 0xCC,
+    console_default = 0x07
+
+};
+
+
+class Shape
+{
+   
+    Color color;
+
+private:
+
+};
+
+
+
+void main()
+{
+    setlocale(LC_ALL, '');
+
+
+}
